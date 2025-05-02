615b6c22ac5e45939a8536caba697cba113c2034
Kolzza <cole.miller2022@gmail.com>
2025-05-02 12:59:26 -0700

    Scraping works for one input website

---
+++ WikiScrape/WikiScrape/spiders/main.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class WebScraper(scrapy.Spider):
+    name = "scraper"
+    start_urls = ['https://en.wikipedia.org/wiki/List_of_search_engines']
+
+    def __init__(self, seeds=None):
+        _seeds = seeds
+        _queue = []
+
+   
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response): 
+        for link in response.css('div.mw-parser-output ul li a'):
+            text = link.css('::text').get()
+            href = link.css('::attr(href)').get()
+            if text and href:
+                self._queue.append(href)
+                yield {
+                    'text': text,
+                    'href': response.urljoin(href)  
+                }  
+
+            
+        
+        
+    

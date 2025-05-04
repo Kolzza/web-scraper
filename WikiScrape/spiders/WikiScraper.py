c008a9616ba87018fda37bbb0fef1d42a7f37b06
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-05-04 03:38:12 -0700

    feat: add global url deduplication

--- WikiScrape/spiders/WikiScraper.py
+++ WikiScrape/spiders/WikiScraper.py
@@ -1,36 +1,34 @@
 import scrapy
 
 class WebScraper(scrapy.Spider):
-    name = "scraper"
-    start_urls = ['https://en.wikipedia.org/wiki/List_of_search_engines']
-    _queue = []
-    def __init__(self, seeds=None):
-        _seeds = seeds
-        _queue = []
+    name="wikipedia"
+    visited=set()
 
-   
     def start_requests(self):
+        self.url_queue = []
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response): 
-        filename = "scraped.txt"
+    def parse(self, response):
+        filename = f"out/{self.name}/{response.url.split('/')[-1]}.html"
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log(f'Saved file {filename}')
 
         for link in response.css('div.mw-parser-output ul li a'):
             text = link.css('::text').get()
             href = link.css('::attr(href)').get()
             if text and href:
                 full_url = response.urljoin(href)
-                self._queue.append(full_url)
+                if full_url not in WebScraper.visited:
+                    self.url_queue.append(full_url)
+                    WebScraper.visited.add(full_url)
                 yield {
                     'text': text,
                     'href': full_url
-                }  
-        while self._queue:
-            next_url = self._queue.pop(0)
+                }
+
+        while self.url_queue:
+            next_url = self.url_queue.pop(0)
             yield scrapy.Request(url=next_url, callback=self.parse)
 
 

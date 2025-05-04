e65b95874ee7ef5dc4358093fd6c9956d9eb7b70
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-05-04 04:03:03 -0700

    build: add shell script to launch crawler

---
+++ crawler.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Installing Dependencies..."
+pip -q install --upgrade pip
+pip -q install -r requirements.txt
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <Seed File> <Number of Pages> <Max Hops> <Output Directory>"
+    exit 1
+fi
+
+if [ -f "$1" ]; then
+    echo "Reading Seed URLs from $1..."
+else
+    echo "Seed File $1 does not exist."
+    exit 1
+fi
+
+if [ ! -d "$4" ]; then
+    echo "Directory $4 not found!"
+    echo "Creating $4/"
+    mkdir -p $4
+fi
+
+echo "Starting Crawler..."
+# python3 WikiScrape/main.py
+echo "Completed!"

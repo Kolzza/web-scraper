10d4a7c28b741bec6b5bd3f6446edd63711dfcd4
BuildTools <unconfigured@null.spigotmc.org>
2025-05-05 11:44:23 -0700

    Test modified spider

--- WikiScrape/spiders/WikiScraper.py
+++ WikiScrape/spiders/WikiScraper.py
@@ -5,26 +5,14 @@ class WebScraper(scrapy.Spider):
     name="wikipedia"
     visited=set()
 
-    def start_requests(self):
-        self.url_queue = []
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
         yield { 
             'body': response.body,
             'url': response.url 
         }
 
-        for link in response.css('div.mw-parser-output ul li a'):
-            text = link.css('::text').get()
-            href = link.css('::attr(href)').get()
-            if text and href:
-                full_url = response.urljoin(href)
-                if full_url not in WebScraper.visited:
-                    self.url_queue.append(full_url)
-                    WebScraper.visited.add(full_url)
-
-        while self.url_queue:
-            next_url = self.url_queue.pop(0)
-            yield scrapy.Request(url=next_url, callback=self.parse)
+        for link in response.css('div.mw-parser-output a::attr(href)'):
+            if link.startswith('/wiki/') and ':' not in link:
+                full_url = response.urljoin(link)
+                if full_url not in self.visited:
+                    yield scrapy.Request(url=full_url, callback=self.parse)

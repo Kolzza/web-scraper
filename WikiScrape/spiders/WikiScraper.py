108c149077bcaf31699489046274899177e859e3
BuildTools <unconfigured@null.spigotmc.org>
2025-05-05 19:39:09 -0700

    temp revert

--- WikiScrape/spiders/WikiScraper.py
+++ WikiScrape/spiders/WikiScraper.py
@@ -5,20 +5,26 @@ class WebScraper(scrapy.Spider):
     name="wikipedia"
     visited=set()
 
+    def start_requests(self):
+        self.url_queue = []
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        url = response.url
-        if url in self.visited:
-            return
-        self.visited.add(url)
-        
         yield { 
             'body': response.body,
-            'url': url
+            'url': response.url 
         }
 
-        for link in response.css('div.mw-parser-output a::attr(href)'):
-            href = link.get()
-            if href and href.startswith('/wiki/') and ':' not in href:
+        for link in response.css('div.mw-parser-output ul li a'):
+            text = link.css('::text').get()
+            href = link.css('::attr(href)').get()
+            if text and href:
                 full_url = response.urljoin(href)
-                if full_url not in self.visited:
-                    yield scrapy.Request(url=full_url, callback=self.parse)
+                if full_url not in WebScraper.visited:
+                    self.url_queue.append(full_url)
+                    WebScraper.visited.add(full_url)
+
+        while self.url_queue:
+            next_url = self.url_queue.pop(0)
+            yield scrapy.Request(url=next_url, callback=self.parse)
